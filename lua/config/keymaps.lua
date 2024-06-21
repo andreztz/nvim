35024e47aae6ecf290e9c3a648955f8cedb6a8a9
André P. Santos <andreztz@gmail.com>
2024-06-21 13:45:55 -0300

    Fix tab for completion

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,9 +1,4 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Disable default keymaps
-vim.keymap.del("n", "<C-Up>")
-vim.keymap.del("n", "<C-Down>")
-vim.keymap.del("n", "<C-Left>")
-vim.keymap.del("n", "<C-Right>")
--- Add any additional keymaps here
+-- -- Add any additional keymaps here
 vim.api.nvim_set_keymap("n", "<F5>", ":Pytest<CR>", { noremap = true, silent = true })
